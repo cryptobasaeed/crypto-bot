63319ce2fbea4016eb4cdb0b69b3de2a9d3c3eeb
cryptobasaeed <saeedhaghbinoriginal@gmail.com>
2026-06-16 16:36:58 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات روشن شد 🚀")
+
+def main():
+    if not TOKEN:
+        print("BOT_TOKEN missing")
+        return
+
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
